1e8de3ba5c7c39f5aa7d014bfb54a44e6332d7a4
Andrea Liboni <liboni.andrea@gmail.com>
2020-02-04 20:47:49 +0100

    Added the generation of table's header

--- bin/templated-projection/code/ricerca.php
+++ bin/templated-projection/code/ricerca.php
@@ -1,36 +1,51 @@
     <?php
       include "connect.php";
-      $ric = $_REQUEST["string"];
-      $query = "SELECT Tipologia,Ragione_sociale,Comune,Provincia,Indirizzo,CAP,Sito_Web,Settore from Aziende";
+      $user = "student";
+      switch ($user) {
+        case 'student':
+          $attributes = "ID,Tipologia,Ragione_sociale,Comune,Provincia,Indirizzo,CAP,Sito_Web,Settore";
+          break;
+        case 'tutor':
+          $attributes = "*";
+          break;
+        default:
+          echo "Utente errato";
+          break;
+      }
+      $query = "SELECT $attributes from Aziende";
       $stmt = $con->prepare( $query );
-      $stmt->execute(array("%$ric%"));
+      $stmt->execute();
       $nRighe = $stmt->rowCount();
 
       if ($nRighe > 0){
-        echo "<table border='1'>";
-        headerTabella();
+        echo "<table>";
+        headerTabella($attributes);
         for ($i=0; $i < $nRighe; $i++) {
           $riga = $stmt->fetch(PDO::FETCH_NUM);
-
-
+          //echo "<pre>";
+          //var_dump($riga);
+          //echo "</pre>";
           echo "<tr>";
-          echo "<td>".($i + 1)."</td>";
           foreach ($riga as $key => $value) {
             echo "<td>$value</td>";
           }
-
           echo "</tr>";
         }
         echo "</table>";
       }else{
-        echo "Nessuna traccia musicale chiamata $ric è presente nell'elenco";
+        echo "Nessuna voce trovata con la query.";
       }
 
-      function headerTabella(){
-            echo "<tr>";
-                echo "<td>N</td>";
-                echo "<td>IdArtist</td>";
-                echo "<td>Name</td>";
-            echo "</tr>";
+      function headerTabella($header){
+        if ($header != "*"){
+          $header = explode(",",$header);
+        }else{
+          $header = ["ID","Tipologia","Ragione_sociale","Comune","Provincia","Indirizzo","CAP","Telefono","Email","Sito_Web","Dipendenti","Data_convenzione","Settore","Codice_ATECO","Descrizione"];
+        }
+        echo "<tr>";
+        foreach ($header as $attribute) {
+          echo "<td>$attribute</td>";
+        }
+        echo "</tr>";
       }
     ?>
