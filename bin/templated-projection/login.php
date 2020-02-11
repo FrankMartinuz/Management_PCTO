5401b17a26e88269e3a0f4cf021c8d58ca954368
AndreaLiboni <liboni.andrea@gmail.com>
2020-02-11 13:08:09 +0100

    hydfdrf

--- bin/templated-projection/login.php
+++ bin/templated-projection/login.php
@@ -39,17 +39,16 @@ isset($_POST["password"])){
 -->
 <html>
 <head>
-  <title>Generic - Projection by TEMPLATED</title>
+  <title>Login</title>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="stylesheet" href="assets/css/main.css" />
 </head>
 <body class="subpage">
-
   <!-- Header -->
   <header id="header">
     <div class="inner">
-      <a href="index.html" class="logo"><strong>Projection</strong> by TEMPLATED</a>
+      <a href="index.html" class="logo"><strong>Managemnt PCTO</strong></a>
       <nav id="nav">
         <a href="index.html">Home</a>
         <a href="generic.html">Generic</a>
@@ -59,31 +58,27 @@ isset($_POST["password"])){
     </div>
   </header>
 
-
-
+  <!-- Body -->
   <section id="three" class="wrapper">
-
-
-
     <div class="inner">
       <header class="align-center">
         <h2>Login</h2>
         <p>Immettere qui le credenziali</p>
       </header>
-<div class="align-center">
-
-</div>
-  <form class="login" action="index.html" method="post">
-    <div class="6u 12u$(xsmall)">
-      <input type="text" name="demo-name" id="demo-name" value="" placeholder="Name" />
+      <hr class="major"/>
+      <!-- form -->
+      <form action="index.html" method="post" align="center">
+        <div align="center" class="12u 12u$">
+          <h3>Inserire l'indirizzo <strong>Email</strong> di Spaggiari</h3>
+          <input type="email" name="demo-name" id="demo-name" value="" placeholder="email" />
+        </div>
+        <div align="center" class="12u 12u$">
+          <h3>Inserire la <strong>password</strong> di Spaggiari</h3>
+          <input type="password" name="demo-email" id="demo-email" value="" placeholder="password" />
+        </div>
+        <input class="button" type="submit" name="Submit" value="LOGIN">
+      </form>
     </div>
-    <div class="6u$ 12u$(xsmall)">
-      <input type="email" name="demo-email" id="demo-email" value="" placeholder="Email" />
-    </div>
-    <input class="button" type="submit" name="Submit" value="LOGIN">
-  </form>
-  </div>
-</div>
 
   </section>
   <!-- Footer -->
